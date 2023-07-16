cea91d0177f79a8442b4c25de76c53f5618059d0
orhan boz <orhanboz1999@gmail.com>
2023-07-16 16:28:20 +0300

    footer done

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,56 +8,53 @@ import './footer.css';
 
 
 function Footer() {
-    const navigate = useNavigate();
 
-    function handleClick() {
-        navigate("/")
-    }
+    return (<div>
 
-    return (
-        <footer>
-            <div className={"footer"}>
-                <div className={"head-footer"}>
-                    <div className={"footer-container"}>
-                        <div className={"logo-footer"}>
-                            <img src={Logo}/>
-                        </div>
-                        <div className={"footer-middle"}>
-                            <div className={"footer-services"}> Hizmetler</div>
-                            <div className={"footer-links"}>
-                                {field("Ortopedik Rehabilitasyon", "/")}
-                                {field("Sportif Rehabilitasyon", "/About")}
-                                {field("Manuel Terapi", "/Contact")}
-                                {field("Koruyucu Fizyoterapi", "https://www.doktortakvimi.com/ersin-buyunlu-2/fizyoterapi-ve-rehabilitasyon/istanbul")}
-                                {field("Geriatrik Rehabilitasyon", "/")}
-                                {field("Evde Fizik Tedavi", "/About")}
-                                {field("G Therapy", "/Contact")}
-                                {field("Klinik Masaj", "https://www.doktortakvimi.com/ersin-buyunlu-2/fizyoterapi-ve-rehabilitasyon/istanbul")}
-                            </div>
-                        </div>
-                        <div className={"footer-information"}>
-                            <div className={"footer-svg"}>
-                                <div className={"footer-icon"}><img src={Location}/></div>
-                                <p className={"footer-location-info"}> Mimar Sinan Mahallesi, Şair Eşref Bulvarı Güneş
-                                    Apartmanı no:80 Kat:2 Daire:5
-                                    Alsancak/Konak/İZMİR</p>
-                            </div>
-                            <div className={"footer-svg"}>
-                                <div className={"footer-icon"}><img src={Phone}/></div>
-                                <p>+90 546 896 28 60</p>
-                            </div>
-                            <div className={"footer-svg footer-mail"}>
-                                <div className={"footer-icon"}><img src={Mail}/></div>
-                                <p> info@fizandsports.com</p>
-                            </div>
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col">
+                        <h4>FizyoNefes</h4>
+                        <ul>
+                            <li><a href="#">Hakkımızda</a></li>
+                            <li><a href="#">Sizden Gelen Yorumlar</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>Hizmetlerimiz</h4>
+                        <ul>
+                            <li><a href="#">Ortopedik Rehabilitasyon</a></li>
+                            <li><a href="#">Sportif Rehabilitasyon</a></li>
+                            <li><a href="#">Manuel Terapi</a></li>
+                            <li><a href="#">Koruyucu Fizyoterapi</a></li>
+                            <li><a href="#">Geriatrik Rehabilitasyon</a></li>
+                            <li><a href="#">Evde Fizik Tedavi</a></li>
+                            <li><a href="#">G Therapy</a></li>
+                            <li><a href="#">Klinik Masaj</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>Ekibimiz</h4>
+                        <ul>
+                            <li><a href="https://www.doktortakvimi.com/ersin-buyunlu-2/fizyoterapi-ve-rehabilitasyon/istanbul">Fzt. Ersin Büyünlü</a></li>
+                            <li><a href="#">Fzt. İrem Büyünlü</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h4>follow us</h4>
+                        <div className="social-links">
+                            <a href="https://www.facebook.com/FizyoNefes"><i className="fab fa-facebook-f"></i></a>
+                            <a href="https://www.youtube.com/@FizyoNefesGTerapiMerkezi"><i className="fab fa-youtube"></i></a>
+                            <a href="https://www.instagram.com/fizyonefess/"><i className="fab fa-instagram"></i></a>
                         </div>
                     </div>
-                    <div className={"all-rights"}>© Tüm Hakları Saklıdır 2023 FizyoNefes</div>
                 </div>
             </div>
-
         </footer>
-    )
+
+
+    </div>)
 }
 
 const field = (fieldName, route) => {
