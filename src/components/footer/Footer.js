0eb6a691d1f090a8fba0eed1487e118fd96dc527
orhan boz <orhanboz1999@gmail.com>
2023-08-06 16:28:19 +0300

    Link Fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,43 +1,38 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
-import Logo from '../../images/icons/fizyonefes_logo.png'
-import Location from '../../images/icons/location.png'
-import Phone from '../../images/icons/phone.png'
-import Mail from '../../images/icons/mail.png'
 import './footer.css';
 
 
 function Footer() {
 
     return (<div>
-
         <footer className="footer">
             <div className="container">
                 <div className="row">
                     <div className="footer-col">
                         <h4>FizyoNefes</h4>
                         <ul>
-                            <li><a href="#">Hakkımızda</a></li>
-                            <li><a href="#">Sizden Gelen Yorumlar</a></li>
+                            <li><a href="/About">Hakkımızda</a></li>
                         </ul>
                     </div>
                     <div className="footer-col">
                         <h4>Hizmetlerimiz</h4>
                         <ul>
-                            <li><a href="#">Ortopedik Rehabilitasyon</a></li>
-                            <li><a href="#">Sportif Rehabilitasyon</a></li>
-                            <li><a href="#">Manuel Terapi</a></li>
-                            <li><a href="#">Koruyucu Fizyoterapi</a></li>
-                            <li><a href="#">Geriatrik Rehabilitasyon</a></li>
-                            <li><a href="#">Evde Fizik Tedavi</a></li>
-                            <li><a href="#">G Therapy</a></li>
-                            <li><a href="#">Klinik Masaj</a></li>
+                            <li><a href="/Orthopedic">Ortopedik Rehabilitasyon</a></li>
+                            <li><a href="/Pulmoner">Pulmoner Rehabilitasyon</a></li>
+                            <li><a href="/ManuelTherapy">Manuel Terapi</a></li>
+                            <li><a href="/Preventive">Koruyucu Fizyoterapi</a></li>
+                            <li><a href="/Geriatrik">Geriatrik Rehabilitasyon</a></li>
+                            <li><a href="/InHomeTherapy">Evde Fizik Tedavi</a></li>
+                            <li><a href="/GTherapy">G Therapy</a></li>
+                            <li><a href="/ClinicalTherapy">Klinik Masaj</a></li>
                         </ul>
                     </div>
                     <div className="footer-col">
                         <h4>Ekibimiz</h4>
                         <ul>
-                            <li><a href="https://www.doktortakvimi.com/ersin-buyunlu-2/fizyoterapi-ve-rehabilitasyon/istanbul">Fzt. Ersin Büyünlü</a></li>
+                            <li><a
+                                href="https://www.doktortakvimi.com/ersin-buyunlu-2/fizyoterapi-ve-rehabilitasyon/istanbul">Fzt.
+                                Ersin Büyünlü</a></li>
                             <li><a href="#">Fzt. İrem Büyünlü</a></li>
                         </ul>
                     </div>
@@ -45,22 +40,15 @@ function Footer() {
                         <h4>follow us</h4>
                         <div className="social-links">
                             <a href="https://www.facebook.com/FizyoNefes"><i className="fab fa-facebook-f"></i></a>
-                            <a href="https://www.youtube.com/@FizyoNefesGTerapiMerkezi"><i className="fab fa-youtube"></i></a>
+                            <a href="https://www.youtube.com/@FizyoNefesGTerapiMerkezi"><i
+                                className="fab fa-youtube"></i></a>
                             <a href="https://www.instagram.com/fizyonefess/"><i className="fab fa-instagram"></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </footer>
-
-
     </div>)
-}
-
-const field = (fieldName, route) => {
-    return <div className={"footer-field"}>
-        <a href={route}>{fieldName}</a>
-    </div>
 }
 
 
